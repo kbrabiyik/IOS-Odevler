587a239c1ee8ba97e926b9c94075c5451a5931ad
küb <75534407+kbrabiyik@users.noreply.github.com>
2022-04-24 02:02:14 +0300

    Add files via upload

---
+++ HW4/HW4/SayfaA_VC.swift
@@ -0,0 +1,22 @@
+//
+//  SayfaA_VC.swift
+//  HW4
+//
+//  Created by KUBRANIN MACİ on 24.04.2022.
+//
+
+import UIKit
+
+class SayfaA_VC: UIViewController {
+
+    @IBOutlet weak var sayfaA_label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func gitB_button(_ sender: Any) {
+    }
+    
+}
---
+++ HW4/HW4/SayfaX_VC.swift
@@ -0,0 +1,22 @@
+//
+//  SayfaX_VC.swift
+//  HW4
+//
+//  Created by KUBRANIN MACİ on 24.04.2022.
+//
+
+import UIKit
+
+class SayfaX_VC: UIViewController {
+
+    @IBOutlet weak var sayfaX_Label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func gitY_button(_ sender: Any) {
+    }
+    
+}
